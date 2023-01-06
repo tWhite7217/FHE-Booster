60c874e773564eecdaebe5253953fa9548a5e076
tWhite7217 <tommywhite525@gmail.com>
2023-01-06 10:51:17 -0500

    Removed outdated scripts.

--- scripts/make_multiple_graphs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(seq $1 $2)
-do
-    
-    ./make_graph.sh random_graph$i
-    sleep 2
-    
-done
--- scripts/test_all_heuristic_on_multiple_graphs.sh
+++ scripts/test_all_heuristic_on_multiple_graphs.sh
@@ -24,21 +24,3 @@ do
     ./list_schedule_multiple.sh ${i}_converted $1 $2 $3 selective_${i}_converted file $j $4
     ((j=j+1))
 done
-
-
-# ./list_schedule_multiple.sh limited $1 $2 $3 limited_min_bootstrapping file -1
-# ./list_schedule_multiple.sh limited $1 $2 $3 limited_num_paths_with_slack heuristic 0
-# ./list_schedule_multiple.sh limited $1 $2 $3 limited_urgency heuristic 1
-# ./list_schedule_multiple.sh limited $1 $2 $3 limited_urgency_and_num_paths heuristic 2
-# ./list_schedule_multiple.sh limited $1 $2 $3 limited_num_paths_minus_slack heuristic 3
-# ./convert_multiple_to_selective.sh $1 $2 list_limited_min_bootstrapping _min_bootstrapping
-# ./convert_multiple_to_selective.sh $1 $2 list_limited_num_paths_with_slack _num_paths_with_slack
-# ./convert_multiple_to_selective.sh $1 $2 list_limited_urgency _urgency
-# ./convert_multiple_to_selective.sh $1 $2 list_limited_urgency_and_num_paths _urgency_and_num_paths
-# ./convert_multiple_to_selective.sh $1 $2 list_limited_num_paths_minus_slack _num_paths_minus_slack
-# ./list_schedule_multiple.sh limited $1 $2 $3 selective_min_bootstrapping_converted file -1
-# ./list_schedule_multiple.sh limited $1 $2 $3 selective_num_paths_with_slack_converted file 0
-# ./list_schedule_multiple.sh limited $1 $2 $3 selective_urgency_converted file 1
-# ./list_schedule_multiple.sh limited $1 $2 $3 selective_urgency_and_num_paths_converted file 2
-# ./list_schedule_multiple.sh limited $1 $2 $3 selective_num_paths_minus_slack_converted file 3
-
