e64e2028c8ff30dbd43c53c646f6444bfe72c95d
tWhite7217 <tommywhite525@gmail.com>
2023-02-02 17:18:26 -0500

    Removed bootstrapping set selection code from ListScheduler.
    This significantly reduces the number of command line arguments. Now, either an lgr file must be provided or "NULL", which means scheduling will be done without bootstrapping. Batching has been removed since its benefits will be much less with bootstrapping paths not being loaded in.

--- CPP_code/list_scheduler.cpp
+++ CPP_code/list_scheduler.cpp
@@ -10,26 +10,18 @@ ListScheduler::ListScheduler(int argc, char **argv)
     operations = input_parser.get_operations();
     operation_type_to_latency_map = input_parser.get_operation_type_to_latency_map();
 
-    if (options.bootstrapping[schedule_index])
+    if (options.lgr_file_path != "NULL")
     {
-        if (options.lgr_file_paths[schedule_index] != "NULL")
-        {
-            lgr_parser.switchIstream(options.lgr_file_paths[schedule_index]);
-            lgr_parser.set_operations(operations);
-            lgr_parser.lex();
-        }
-        else
-        {
-            BootstrappingPathGenerator path_generator(operations, lgr_parser.used_selective_model, options.num_levels);
-            bootstrapping_paths = path_generator.get_bootstrapping_paths(options.dag_file_path);
-        }
+        lgr_parser.switchIstream(options.lgr_file_path);
+        lgr_parser.set_operations(operations);
+        lgr_parser.lex();
     }
 
-    const auto num_threads = options.num_threads[schedule_index];
+    const auto num_threads = options.num_threads;
     if (num_threads > 0)
     {
         core_schedules.assign(num_threads, "");
-        for (auto i = 1; i <= options.num_threads[schedule_index]; i++)
+        for (auto i = 1; i <= options.num_threads; i++)
         {
             core_availability[i] = true;
         }
@@ -41,111 +33,6 @@ ListScheduler::ListScheduler(int argc, char **argv)
         start_bootstrapping_ready_operations = [this]()
         { start_bootstrapping_ready_operations_for_unlimited_model(); };
     }
-}
-
-OperationList ListScheduler::get_operations()
-{
-    return operations;
-}
-
-void ListScheduler::update_earliest_start_time(OperationPtr operation)
-{
-    for (auto parent : operation->parent_ptrs)
-    {
-        int parent_latency = operation_type_to_latency_map[parent->type];
-        if (vector_contains_element(parent->child_ptrs_that_receive_bootstrapped_result, operation))
-        {
-            parent_latency += bootstrapping_latency;
-        }
-        int possible_est =
-            parent->earliest_start_time +
-            parent_latency;
-        if (possible_est > operation->earliest_start_time)
-        {
-            operation->earliest_start_time = possible_est;
-        }
-    }
-}
-
-int ListScheduler::get_earliest_possible_program_end_time()
-{
-    int earliest_possible_program_end_time = 0;
-    for (auto operation : operations)
-    {
-        auto latency = operation_type_to_latency_map[operation->type];
-        int operation_earliest_end_time = operation->earliest_start_time + latency;
-        if (operation_earliest_end_time > earliest_possible_program_end_time)
-        {
-            earliest_possible_program_end_time = operation_earliest_end_time;
-        }
-    }
-    return earliest_possible_program_end_time;
-}
-
-void ListScheduler::update_latest_start_time(OperationPtr operation, int earliest_possible_program_end_time)
-{
-    operation->latest_start_time = std::numeric_limits<decltype(operation->latest_start_time)>::max();
-
-    if (operation->child_ptrs.empty())
-    {
-        operation->latest_start_time =
-            earliest_possible_program_end_time - operation_type_to_latency_map[operation->type];
-    }
-    else
-    {
-        for (auto child : operation->child_ptrs)
-        {
-            int child_latency = operation_type_to_latency_map[child->type];
-            if (operation_is_bootstrapped(child))
-            {
-                child_latency += bootstrapping_latency;
-            }
-            int possible_lst =
-                child->latest_start_time -
-                child_latency;
-            if (possible_lst < operation->latest_start_time)
-            {
-                operation->latest_start_time = possible_lst;
-            }
-        }
-    }
-}
-
-void ListScheduler::update_all_ESTs_and_LSTs()
-{
-    // auto operations_in_topological_order = get_operations_in_topological_order();
-    // TGFF puts operations in topological order so we do not need to do that here
-    auto operations_in_topological_order = operations;
-
-    for (auto operation : operations_in_topological_order)
-    {
-        update_earliest_start_time(operation);
-    }
-
-    int earliest_program_end_time = get_earliest_possible_program_end_time();
-
-    auto operations_in_reverse_topological_order = operations_in_topological_order;
-    std::reverse(operations_in_reverse_topological_order.begin(), operations_in_reverse_topological_order.end());
-
-    for (auto operation : operations_in_reverse_topological_order)
-    {
-        update_latest_start_time(operation, earliest_program_end_time);
-    }
-}
-
-int ListScheduler::update_all_ranks()
-{
-    int max = 0;
-    for (auto &operation : operations)
-    {
-        int slack = operation->latest_start_time - operation->earliest_start_time;
-        operation->rank = slack;
-        if (slack > max)
-        {
-            max = slack;
-        }
-    }
-    return max;
 }
 
 void ListScheduler::initialize_pred_count()
@@ -183,7 +70,7 @@ void ListScheduler::generate_start_times_and_solver_latency()
     {
         update_simulation_state();
 
-        if (options.num_threads[schedule_index] > 0)
+        if (options.num_threads > 0)
         {
             mark_cores_available(finished_bootstrapping_operations);
         }
@@ -279,14 +166,6 @@ void ListScheduler::start_ready_operations()
 
             auto core_schedules_index = best_core - 1;
 
-            // for (auto parent : operation->parent_ptrs)
-            // {
-            //     if (parent->core_num != best_core)
-            //     {
-            //         std::string wait_line = "wait " + std::to_string(parent->id) + "\n";
-            //         core_schedules[best_core] += wait_line;
-            //     }
-            // }
             std::string result_var = " c" + std::to_string(operation->id);
 
             std::string arg1;
@@ -452,7 +331,7 @@ bool ListScheduler::core_is_available(int core_num)
 void ListScheduler::write_lgr_like_format()
 {
     std::ofstream output_file;
-    output_file.open(options.output_file_paths[schedule_index] + ".lgr");
+    output_file.open(options.output_file_path + ".lgr");
 
     output_file << "Objective value: " << solver_latency << ".0" << std::endl;
 
@@ -506,7 +385,7 @@ void ListScheduler::write_lgr_like_format()
 void ListScheduler::write_assembly_like_format()
 {
     std::ofstream output_file;
-    output_file.open(options.output_file_paths[schedule_index] + ".sched");
+    output_file.open(options.output_file_path + ".sched");
 
     for (auto schedule : core_schedules)
     {
@@ -514,143 +393,19 @@ void ListScheduler::write_assembly_like_format()
     }
 }
 
-void ListScheduler::choose_operations_to_bootstrap()
-{
-    auto count = 0;
-    while (!bootstrapping_paths_are_satisfied(bootstrapping_paths))
-    {
-        max_num_paths = update_num_paths_for_every_operation();
-        if (options.slack_weight[schedule_index] != 0)
-        {
-            update_all_ESTs_and_LSTs();
-            max_slack = update_all_ranks();
-        }
-        if (options.urgency_weight[schedule_index] != 0)
-        {
-            update_all_bootstrap_urgencies();
-        }
-        choose_operation_to_bootstrap_based_on_score();
-    }
-}
-
-void ListScheduler::update_all_bootstrap_urgencies()
-{
-    for (auto &operation : operations)
-    {
-        operation->bootstrap_urgency = -1;
-    }
-
-    auto count = 0;
-    for (auto &path : bootstrapping_paths)
-    {
-        if (path_is_urgent(path))
-        {
-            count++;
-            auto path_size = path.size();
-            for (double i = 0; i < path_size; i++)
-            {
-                path[i]->bootstrap_urgency = std::max(
-                    path[i]->bootstrap_urgency, (i + 1) / path_size);
-            }
-        }
-    }
-    if (count == 0)
-    {
-        std::cout << "here" << std::endl;
-    }
-}
-
-void ListScheduler::choose_operation_to_bootstrap_based_on_score()
-{
-    auto max_score = -1;
-    OperationPtr max_score_operation;
-    for (auto &operation : operations)
-    {
-        if (!operation_is_bootstrapped(operation))
-        {
-            auto score = get_score(operation);
-            if (score > max_score)
-            {
-                max_score = score;
-                max_score_operation = operation;
-            }
-        }
-    }
-
-    max_score_operation->child_ptrs_that_receive_bootstrapped_result = max_score_operation->child_ptrs;
-}
-
-double ListScheduler::get_score(OperationPtr operation)
-{
-    if (operation->num_unsatisfied_paths == 0)
-    {
-        return 0;
-    }
-
-    double normalized_num_paths = ((double)operation->num_unsatisfied_paths) / max_num_paths;
-    double normalized_slack = ((double)operation->rank) / max_slack;
-
-    return std::max(options.segments_weight[schedule_index] * normalized_num_paths +
-                        options.slack_weight[schedule_index] * normalized_slack +
-                        options.urgency_weight[schedule_index] * operation->bootstrap_urgency,
-                    0.0);
-}
-
-int ListScheduler::update_num_paths_for_every_operation()
-{
-    for (auto &operation : operations)
-    {
-        operation->num_unsatisfied_paths = 0;
-    }
-
-    for (auto path : bootstrapping_paths)
-    {
-        if (lgr_parser.used_selective_model)
-        {
-            path.pop_back();
-        }
-
-        if (!bootstrapping_path_is_satisfied(path))
-        {
-            for (auto &operation : path)
-            {
-                operation->num_unsatisfied_paths++;
-            }
-        }
-    }
-
-    int max = 0;
-
-    for (auto &operation : operations)
-    {
-        if (operation->num_unsatisfied_paths > max)
-        {
-            max = operation->num_unsatisfied_paths;
-        }
-    }
-
-    return max;
-}
-
 void ListScheduler::perform_list_scheduling()
 {
-    std::cout << "here1" << std::endl;
-    if (options.bootstrapping[schedule_index] && options.lgr_file_paths[schedule_index] == "NULL")
-    {
-        choose_operations_to_bootstrap();
-    }
-    std::cout << "here2" << std::endl;
-    update_all_ESTs_and_LSTs();
-    std::cout << "here3" << std::endl;
-    update_all_ranks();
-    std::cout << "here4" << std::endl;
+    update_all_ESTs_and_LSTs(operations, operation_type_to_latency_map);
+    update_all_slacks(operations);
+    std::cout << "Generating lgr..." << std::endl;
     generate_start_times_and_solver_latency();
-    std::cout << "here5" << std::endl;
-    if (options.num_threads[schedule_index] > 0)
+    std::cout << "Done." << std::endl;
+    std::cout << "Generating sched..." << std::endl;
+    if (options.num_threads > 0)
     {
         generate_core_assignments();
     }
-    std::cout << "here6" << std::endl;
+    std::cout << "Done." << std::endl;
 }
 
 void ListScheduler::update_pred_count()
@@ -687,8 +442,7 @@ void ListScheduler::parse_args(int argc, char **argv)
     }
 
     options.dag_file_path = argv[1];
-    options.output_file_paths = split_string_by_character(argv[2], ',');
-    num_schedules = options.output_file_paths.size();
+    options.output_file_path = argv[2];
 
     std::string options_string;
     for (auto i = 3; i < argc; i++)
@@ -696,40 +450,25 @@ void ListScheduler::parse_args(int argc, char **argv)
         options_string += std::string(argv[i]) + " ";
     }
 
-    auto num_levels_string = get_arg(options_string, "-l", "--num-levels", help_info);
-    if (!num_levels_string.empty())
+    auto num_threads_string = get_arg(options_string, "-t", "--num-threads", help_info);
+    if (!num_threads_string.empty())
     {
-        options.num_levels = stoi(num_levels_string);
+        options.num_threads = std::stoi(num_threads_string);
     }
 
-    const std::function<int(std::string)> stoi_function = [](std::string str)
-    { return std::stoi(str); };
-
-    const std::function<std::string(std::string)> stos_function = [](std::string str)
-    { return str; };
-
-    const std::function<bool(std::string)> stob_function = [](std::string str)
-    { return bool_arg_converter(str); };
-
-    options.num_threads = get_list_arg(options_string, "-t", "--num-threads", help_info, num_schedules, 1, stoi_function);
-    options.bootstrapping = get_list_arg(options_string, "-b", "--bootstrap", help_info, num_schedules, true, stob_function);
-    options.lgr_file_paths = get_list_arg(options_string, "-i", "--input-lgr", help_info, num_schedules, std::string("NULL"), stos_function);
-    options.segments_weight = get_list_arg(options_string, "-s", "--segments-weight", help_info, num_schedules, 0, stoi_function);
-    options.slack_weight = get_list_arg(options_string, "-r", "--slack-weight", help_info, num_schedules, 0, stoi_function);
-    options.urgency_weight = get_list_arg(options_string, "-u", "--urgency-weight", help_info, num_schedules, 0, stoi_function);
+    auto lgr_arg = get_arg(options_string, "-i", "--input-lgr", help_info);
+    if (!lgr_arg.empty())
+    {
+        options.lgr_file_path = lgr_arg;
+    }
 }
 
 void ListScheduler::print_options()
 {
     std::cout << "dag_file_path: " << options.dag_file_path << std::endl;
-    // std::cout << "output_file_path: " << options.output_file_path << std::endl;
-    // std::cout << "lgr_file_path: " << options.lgr_file_path << std::endl;
-    // std::cout << "no_bootstrapping: " << (options.no_bootstrapping ? "yes" : "no") << std::endl;
-    // std::cout << "num_threads: " << options.num_threads << std::endl;
-    // std::cout << "num_levels: " << options.num_levels << std::endl;
-    // std::cout << "segments_weight: " << options.segments_weight << std::endl;
-    // std::cout << "slack_weight: " << options.slack_weight << std::endl;
-    // std::cout << "urgency_weight: " << options.urgency_weight << std::endl;
+    std::cout << "output_file_path: " << options.output_file_path << std::endl;
+    std::cout << "lgr_file_path: " << options.lgr_file_path << std::endl;
+    std::cout << "num_threads: " << options.num_threads << std::endl;
 }
 
 int main(int argc, char **argv)
--- CPP_code/list_scheduler.h
+++ CPP_code/list_scheduler.h
@@ -14,15 +14,12 @@ class ListScheduler
 public:
     ListScheduler(int, char **);
 
-    OperationList get_operations();
+    // OperationList get_operations();
 
     void perform_list_scheduling();
 
-    void update_all_ESTs_and_LSTs();
-    int update_all_ranks();
     void generate_start_times_and_solver_latency();
     void generate_core_assignments();
-    void choose_operations_to_bootstrap();
 
     void write_lgr_like_format();
     void write_assembly_like_format();
@@ -30,68 +27,25 @@ public:
 private:
     const std::string help_info = R"(
 Usage: ./list_scheduler <dag_file> 
-                        <output_file_1>[,<output_file_2>,<output_file_n>]
+                        <output_file>
                         [<options>]
 
 Options:
   -t <int>, --num-threads=<int>
     The number of threads on which operations may be scheduled.
-  -b <y/n>, --bootstrap=<y/n>
-    Setting to "y" creates a schedule with bootstrapping, while
-    setting to "n" creates a schedule without bootstrapping.
-    Defaults to "y".
+    Defaults to 1.
   -i <file/"NULL">, --input-lgr=<file/"NULL">
     A path to a .lgr file specifying a set of operations to bootstrap.
-    Setting to "NULL" (the default), means operations to bootstrap
-    must be chosen before scheduling. Ignored if -b/--bootstrap is set
-    to "n".
-  -l <int>, --num-levels=<int>
-    The number of levels between bootstraps, also called the noise
-    threshold. Defaults to 9. Ignored if the -b/--bootstrap option is
-    set to "n" or the -i/--input-lgr option is not "NULL".
-  Weights:
-    The following options apply weights to certain attributes that are
-    used in choosing operations to bootstrap. These options are
-    ignored if the -b/--bootstrap option is set to "n" or the
-    -i/--input-lgr option is not "NULL". All default to 0.
-      -s <int>, --segments-weight=<int>
-        This attribute correlates to the number of unsatisfied 
-        bootstrapping segments containing the operation.
-      -r <int>, --slack-weight=<int>
-        This attribute correlates to the difference between the
-        operation's latest and earliest starting times.
-      -u <int>, --urgency-weight=<int>
-        This attribute correlates to the current bootstrapping
-        urgency level of the operation.
-        
-Batching:
-  Notice that multiple output files may be specified as a comma-
-  separated list. This is so multiple schedules can be created for a
-  single graph without loading that graph's bootstrapping segments
-  multiple times. To accomodate this, all used options except
-  -l/--num-levels, must also be comma-separated lists, of the same
-  length as the list of output files. The option -l/--num-levels is
-  an exception because a graph's bootstrapping segments differ for
-  different numbers of levels.)";
+    Setting to "NULL" means scheduling will be performed without
+    bootstrapping. Defaults to "NULL".)";
 
     struct Options
     {
         std::string dag_file_path;
-        int num_levels = 9;
-        std::vector<std::string> output_file_paths;
-        std::vector<std::string> lgr_file_paths;
-        std::vector<bool> bootstrapping;
-        std::vector<int> num_threads;
-        std::vector<int> segments_weight;
-        std::vector<int> slack_weight;
-        std::vector<int> urgency_weight;
+        std::string output_file_path;
+        std::string lgr_file_path = "NULL";
+        int num_threads = 1;
     } options;
-
-    size_t num_schedules;
-    size_t schedule_index = 0;
-
-    int max_num_paths;
-    int max_slack;
 
     int solver_latency;
 
@@ -105,40 +59,28 @@ Batching:
     std::vector<OperationList> bootstrapping_paths;
     LGRParser lgr_parser;
 
-    struct PriorityCmp
+    struct SlackCmp
     {
         bool operator()(const OperationPtr &a, const OperationPtr &b) const
         {
-            return a->rank < b->rank;
+            return a->slack < b->slack;
         }
     };
 
     std::map<OperationPtr, int> pred_count;
     std::map<OperationPtr, int> running_operations;
     std::map<OperationPtr, int> bootstrapping_operations;
-    std::multiset<OperationPtr, PriorityCmp> prioritized_unstarted_operations;
+    std::multiset<OperationPtr, SlackCmp> prioritized_unstarted_operations;
     OperationList ready_operations;
     int clock_cycle;
 
     std::unordered_set<OperationPtr> finished_running_operations;
     std::unordered_set<OperationPtr> finished_bootstrapping_operations;
 
-    struct RankCmp
-    {
-        bool operator()(const OperationPtr &a, const OperationPtr &b) const
-        {
-            return a->rank < b->rank;
-        }
-    };
-    std::multiset<OperationPtr, RankCmp> bootstrapping_queue;
+    std::multiset<OperationPtr, SlackCmp> bootstrapping_queue;
 
     std::function<void()> start_bootstrapping_ready_operations;
 
-    void update_earliest_start_time(OperationPtr);
-    int get_earliest_possible_program_end_time();
-    void update_latest_start_time(OperationPtr, int);
-    void update_all_bootstrap_urgencies();
-    int update_num_paths_for_every_operation();
     void initialize_pred_count();
     void update_ready_operations();
     std::unordered_set<OperationPtr> handle_started_operations(std::map<OperationPtr, int> &);
@@ -153,8 +95,6 @@ Batching:
     int get_available_core_num();
     bool core_is_available(int);
     bool program_is_not_finished();
-    void choose_operation_to_bootstrap_based_on_score();
-    double get_score(OperationPtr);
     std::string get_constant_arg(OperationPtr, size_t);
     std::string get_variable_arg(OperationPtr, size_t);
     void mark_cores_available(std::unordered_set<OperationPtr> &);
